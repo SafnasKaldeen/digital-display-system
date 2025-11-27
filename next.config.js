cc723ae46ca3e049fe0f5231c0e76066ca204b79
SafnasKaldeen <safnaskm.20@uom.lk>
2025-11-27 23:22:35 +0530

    k

--- next.config.js
+++ next.config.js
@@ -1,12 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone", // needed for server components / App Router
+  output: "standalone",
   images: {
-    unoptimized: true, // optional, avoids Netlify image optimization issues
+    unoptimized: true,
   },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "all",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+        ],
+      },
+    ];
   },
 };
 
