22ef1e43a8d58b93644217213ef6479e3e3ee20e
SafnasKaldeen <safnaskm.20@uom.lk>
2025-11-27 22:28:55 +0530

    netlify

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type React from "react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Analytics } from "@vercel/analytics/next";
 import { AuthProvider } from "@/providers/auth-provider";
 import "./globals.css";
 
