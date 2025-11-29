77070885a7662280d586ce4f6efa265c6dbb8277
SafnasKaldeen <safnaskm.20@uom.lk>
2025-11-29 16:35:28 +0530

    v2.0

--- components/loading-spinner.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-export function LoadingSpinner() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-background">
-      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-primary"></div>
-    </div>
-  )
-}
--- types/islamic.ts
+++
@@ -1,5 +0,0 @@
-export interface IslamicDate {
-  day: number
-  month: number
-  year: number
-}
